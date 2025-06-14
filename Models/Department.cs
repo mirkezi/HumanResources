bc8d37a0fb6e05aa5203573b98ca1f87bcbd62da
mirkezi <mirko.venali@gmail.com>
2025-06-14 21:51:12 +0200

    Implemented models Employee / Department with navigation properties

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+namespace Hr.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; } // Navigation property
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace Hr.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; } // Navigation property
+    }
+}
